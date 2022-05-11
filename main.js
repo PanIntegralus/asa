f618a9d9ba6d7f6eaf985bb882c76e6b4dd4e6ab
PanIntegralus <42538693+PanIntegralus@users.noreply.github.com>
2022-05-11 18:55:30 +0200

    optional parameters

--- main.js
+++ main.js
@@ -16,48 +16,52 @@ app.get('/api', (req, res) => {
 // sitios que se pueden scrapear
 var availableSites = config.available_services;
 
-app.get('/api/scrap/:site/:input', (req, res) => {
+app.get('/api/scrap/:site?/:input?', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     req.params;
-    if (availableSites.includes(req.params.site)) {
-        var url;
-        switch (req.params.site) {
-            case "monoschinos2":
-                url = "https://monoschinos2.com/buscar?q="+req.params.input;
-
-                axios.get(url)
-                .then (function (response) {
-                    var body = response.data
-                    // console.log(body)
-                    let $ = cheerio.load(body);
-                    let animelist = []
-                    $('div.row a[href]').each(function(index) {
-                        if (index == 30) {return false}
-                        console.log(index);
-                        title = $(this).children().children('.seriesdetails').children('.seristitles').text()
-                        url = $(this).attr('href');
-                        img = $(this).children().children().children().attr('src');
-                        var index = {
-                            "title": title,
-                            "url": url,
-                            "img": img
-                        }
-                        animelist.push(index);
-                        // count[url] = $(this).attr('href')
-                        // data.push(count)
-                        // count++
-                    });
-                        res.json({animelist});
-                })
-                .catch (function (error) {console.log(error)});
-
-            case "animeflv":
-                url = "https://www3.animeflv.net/browse?q=";
-            
-            default:
-                break;
-        }
-    } else { res.json({}) }
+    if (!req.params.site) {
+        res.json({});
+    } else {
+        if (availableSites.includes(req.params.site)) {
+            var url;
+            switch (req.params.site) {
+                case "monoschinos2":
+                    url = "https://monoschinos2.com/buscar?q="+req.params.input;
+    
+                    axios.get(url)
+                    .then (function (response) {
+                        var body = response.data
+                        // console.log(body)
+                        let $ = cheerio.load(body);
+                        let animelist = []
+                        $('div.row a[href]').each(function(index) {
+                            if (index == 30) {return false}
+                            console.log(index);
+                            title = $(this).children().children('.seriesdetails').children('.seristitles').text()
+                            url = $(this).attr('href');
+                            img = $(this).children().children().children().attr('src');
+                            var index = {
+                                "title": title,
+                                "url": url,
+                                "img": img
+                            }
+                            animelist.push(index);
+                            // count[url] = $(this).attr('href')
+                            // data.push(count)
+                            // count++
+                        });
+                            res.json({animelist});
+                    })
+                    .catch (function (error) {console.log(error)});
+    
+                case "animeflv":
+                    url = "https://www3.animeflv.net/browse?q=";
+                
+                default:
+                    break;
+            }
+        } else { res.json({}) };
+    };
 });
 
 app.listen(port, () => {
