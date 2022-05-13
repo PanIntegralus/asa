22d9d7099e4257ebd0ad315733914e6f34ff2239
PanIntegralus <42538693+PanIntegralus@users.noreply.github.com>
2022-05-13 10:54:51 +0200

    test

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ const asa = require('./main');
 
 console.log(asa.availableSources());
 
-asa.getAnime("animeflv", "86")
+asa.getAnime("animeflv", "serial experiments")
     .then(res => console.log(res));
 
-asa.getAnime("animeflv", "serial experiments").then(res => {
-    asa.getEpisodes("animeflv", res[0].url).then(res => {
-        console.log(res)
-    });
-});
+// asa.getAnime("animeflv", "serial experiments").then(res => {
+//     asa.getEpisodes("animeflv", res[0].url).then(res => {
+//         console.log(res)
+//     });
+// });
