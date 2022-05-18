fb053f8dd132e945d6a838c90e472ee4fc644ad2
PanIntegralus <42538693+PanIntegralus@users.noreply.github.com>
2022-05-18 23:44:04 +0200

    added hentai website temporary

---
+++ controllers/hentaila.js
@@ -0,0 +1,79 @@
+const axios = require('axios');
+const res = require('express/lib/response');
+const cheerio = require('cheerio').default;
+
+var srcurl = "https://hentaila.tv";
+
+module.exports.getAnime = async function (inputsearch) {
+    var body = await axios.get(srcurl+"/?s"+inputsearch, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/5351 (KHTML, like Gecko) Chrome/40.0.852.0 Mobile Safari/5351',
+        }
+    })
+    .then (function (response) {
+        return response.data;
+    }) .catch (function (error) {console.log(error)});
+
+    if (typeof body === 'string' || body instanceof String) {
+        var $ = cheerio.load(body);
+        var animelist = []
+        $('div.justify-content-md-center a.card__cover').each(function(index) {
+            if (index == 30) {return false};
+
+            title = $(this).children('h3').text();
+            url = $(this).attr('href');
+            img = $(this).children('img').attr('src');
+
+            var index = {
+                "title": title,
+                "url": url,
+                "img": img
+            };
+            animelist.push(index);
+        });
+        return animelist;
+    } else {return [];}
+}
+
+// module.exports.getEpisodes = async function(animeURL) {
+//     var body = await axios.get(animeURL)
+//     .then (function (response) {
+//         return response.data;
+//     }) .catch (function (error) {console.log(error)});
+
+//     var $ = cheerio.load(body);
+    
+//     var episodeList = []
+//     $('ul#episodeList li').each(function (index) {
+//         url = srcurl+$(this).children('a').attr('href');
+//         var index = {
+//             "url": url
+//         };
+//         episodeList.push(index);
+//     });
+
+//     return episodeList.reverse();
+// }
+
+// module.exports.getStreaming = async function(episodeURL) {
+//     var body = await axios.get(episodeURL)
+//     .then (function (response) {
+//         return response.data;
+//     }) .catch (function (error) {console.log(error)});
+
+//     $ = cheerio.load(body);
+
+//     var streamingList = []
+//     $('ul.CapiTnv li').each(function(index) {
+//         var data_video = $(this).attr('data-video');
+//         var servicename = $(this).attr('title');
+//         if (servicename=="Our Server") {} else {
+//             var index = {
+//                 "servicename": servicename,
+//                 "url": data_video
+//             };
+//             streamingList.push(index);
+//         }
+//     });
+//     return streamingList;
+// }
