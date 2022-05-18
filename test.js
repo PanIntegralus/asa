49586e08b9d1068d66ce69a186e3095aa8694aa4
PanIntegralus <42538693+PanIntegralus@users.noreply.github.com>
2022-05-18 23:34:38 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,12 @@ const asa = require('./main');
 console.log(asa.availableSources());
 
 asa.getAnime("animeflv", "86").then(res => {
+    console.log(res);
     asa.getEpisodes("animeflv", res[0].url).then(res => {
         console.log(res);
+        asa.getStreaming("animeflv", res[7].url).then(res => {
+            console.log(res);
+        });
     })
     .catch(error => {
         console.log(error);
